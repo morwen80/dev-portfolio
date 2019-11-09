df68423d9e49f044b5f91ab053e9caff3a2ba518
morwen80 <ribesnero80@gmail.com>
2019-11-09 20:18:45 +0000

    reduced size of modals and added Kronikles thumbnail

--- index.js
+++ index.js
@@ -1,54 +1,50 @@
 // $(document).ready(function() {
 
+
+
+// link the "See my Work" button to the portfolio section
+$('#see-my-work').click(function() {
+  $(window.location.href="#projects")
+})
+
+
+  // changing cv button background color on click
   $('#cvBtn').click(function() {
     $(this).css('backgroundColor', '#e0bfc3');
   });
 
-  const debounce = (func, wait=20, immediate=true) => {
-	var timeout;
-	return function() {
-		var context = this, args = arguments;
-		var later = function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		};
-		var callNow = immediate && !timeout;
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
-		if (callNow) func.apply(context, args);
-	};
-};
-
-
-// const sliderImages = $('.slide');
-//
-// jQuery.each( sliderImages, function() {
-//
-//   const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
-//   const imageBottom = sliderImage.offsetTop + sliderImage.height;
-//    const isHalfShown = slideInAt > sliderImage.offsetTop;
-//    const isNotScrolledPast = window.scrollY < imageBottom;
-//    if(isHalfShown && isNotScrolledPast) {
-//      sliderImage.classList.add('slideActive')
-//    } else {
-//      sliderImage.classList.remove('sliceDctive')
-//    }
-//  })
-
 
 // PROJECT NATIGATION FILTER
+// $('#all-projects').click(function() {
+//   $('.module').fadeIn().css("display", "flex")
+// });
+//
+// $('#id-react').click(function() {
+//   $('.projectReact').fadeIn().css("display", "flex")
+//   $('.projectGrid div').filter(".projectBootstrap").fadeOut().css("display", "none");
+// });
+//
+// $('#id-bootstrap').click(function() {
+//   $('.projectBootstrap').fadeIn().css("display", "flex")
+//   $('.projectGrid div').filter(".projectReact").fadeOut().css("display", "none");
+// });
+
+
+
+// SECOND TRY
 $('#all-projects').click(function() {
-  $('.module').fadeIn().css("display", "flex")
+  $('.projectCard').fadeIn().css("display", "block")
+
 });
 
 $('#id-react').click(function() {
-  $('.projectReact').fadeIn().css("display", "flex")
-  $('.projectGrid div').filter(".projectBootstrap").fadeOut().css("display", "none");
+  $('.projectReact').fadeIn().css("display", "block")
+  $('.projectGrid').filter(".projectBootstrap").fadeOut().css("display", "none");
 });
 
 $('#id-bootstrap').click(function() {
-  $('.projectBootstrap').fadeIn().css("display", "flex")
-  $('.projectGrid div').filter(".projectReact").fadeOut().css("display", "none");
+  $('.projectBootstrap').fadeIn().css("display", "block")
+  $('.projectGrid').filter(".projectReact").fadeOut().css("display", "none");
 });
 
 
